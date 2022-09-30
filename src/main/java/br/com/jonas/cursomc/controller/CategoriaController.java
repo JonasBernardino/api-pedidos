47aaa73e02dc7691849de472ddeb6bed44c5c475
jolima <jolima@deloitte.com>
2022-09-30 17:47:46 -0300

    Paginação

--- src/main/java/br/com/jonas/cursomc/controller/CategoriaController.java
+++ src/main/java/br/com/jonas/cursomc/controller/CategoriaController.java
@@ -4,6 +4,9 @@ import br.com.jonas.cursomc.domain.Categoria;
 import br.com.jonas.cursomc.dto.CategoriaDTO;
 import br.com.jonas.cursomc.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,7 +23,7 @@ public class CategoriaController {
     private CategoriaService categoriaService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+    public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(categoriaService.findById(id));
     }
 
@@ -32,23 +35,34 @@ public class CategoriaController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> save(@RequestBody Categoria obj){
+    public ResponseEntity<Void> save(@RequestBody Categoria obj) {
         obj = categoriaService.save(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria obj){
+    public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria obj) {
         obj.setId(id);
         obj = categoriaService.update(obj);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Integer id){
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/page", method=RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value="page", defaultValue="0") Integer page,
+            @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+            @RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+            @RequestParam(value="direction", defaultValue="DESC") String direction) {
+        Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
--- src/main/java/br/com/jonas/cursomc/service/CategoriaService.java
+++ src/main/java/br/com/jonas/cursomc/service/CategoriaService.java
@@ -6,6 +6,9 @@ import br.com.jonas.cursomc.service.exceptions.DataIntegrityException;
 import br.com.jonas.cursomc.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -46,4 +49,10 @@ public class CategoriaService {
             throw new DataIntegrityException("Não é possível excluir a categoria que contem produtos");
         }
     }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
+    }
+
 }
